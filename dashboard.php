190f9ab71173639a4f6cbf6cd2e776a2a4bb03e6
divyanshu59 <greatdivyanshu59@gmail.com>
2019-10-31 21:18:15 +0530

    Profile Page Link Added

--- dashboard.php
+++ dashboard.php
@@ -37,7 +37,7 @@ $sql = "SELECT * FROM `users` WHERE `username` = '$username' ";
     <header>
         <span id="mobile" onclick="opennav()"><i class="fa fa-bars" aria-hidden="true"></i></span>
         <span id="sitetitle"><?php echo $SiteName; ?></span>
-        <span id="user"><img src="assets/img/avatar.png"></span>
+        <span id="user"><a href='profile.php'><img src="assets/img/avatar.png"></a></span>
         <span id="username"><?php echo $username ?></span>
         <span id="credits">C: <?php echo $credits ?></span>
         
